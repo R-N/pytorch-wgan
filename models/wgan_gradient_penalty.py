d64d1d71fb01ed802489494d02f71d024826b656
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-24 18:00:20 +0700

    Update wgan_gradient_penalty.py

--- models/wgan_gradient_penalty.py
+++ models/wgan_gradient_penalty.py
@@ -23,17 +23,17 @@ class Generator(torch.nn.Module):
         self.main_module = nn.Sequential(
             # Z latent vector 100
             nn.ConvTranspose2d(in_channels=100, out_channels=1024, kernel_size=4, stride=1, padding=0),
-            nn.BatchNorm2d(num_features=1024),
+            #nn.BatchNorm2d(num_features=1024),
             nn.ReLU(True),
 
             # State (1024x4x4)
             nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=512),
+            #nn.BatchNorm2d(num_features=512),
             nn.ReLU(True),
 
             # State (512x8x8)
             nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=256),
+            #nn.BatchNorm2d(num_features=256),
             nn.ReLU(True),
 
             # State (256x16x16)
