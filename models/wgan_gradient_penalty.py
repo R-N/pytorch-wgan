9352f0e6f120597ad01af84d4daa445ee28069e2
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-24 18:54:50 +0700

    parametrize generator norm

--- models/wgan_gradient_penalty.py
+++ models/wgan_gradient_penalty.py
@@ -15,29 +15,32 @@ import numpy as np
 SAVE_PER_TIMES = 100
 
 class Generator(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, norm=False):
         super().__init__()
         # Filters [1024, 512, 256]
         # Input_dim = 100
         # Output_dim = C (number of channels)
-        self.main_module = nn.Sequential(
+        layers = [
             # Z latent vector 100
             nn.ConvTranspose2d(in_channels=in_channels, out_channels=1024, kernel_size=4, stride=1, padding=0),
-            #nn.BatchNorm2d(num_features=1024),
+            nn.BatchNorm2d(num_features=1024) if norm else None,
             nn.ReLU(True),
 
             # State (1024x4x4)
             nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=4, stride=2, padding=1),
-            #nn.BatchNorm2d(num_features=512),
+            nn.BatchNorm2d(num_features=512) if norm else None,
             nn.ReLU(True),
 
             # State (512x8x8)
             nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1),
-            #nn.BatchNorm2d(num_features=256),
+            nn.BatchNorm2d(num_features=256) if norm else None,
             nn.ReLU(True),
 
             # State (256x16x16)
-            nn.ConvTranspose2d(in_channels=256, out_channels=out_channels, kernel_size=4, stride=2, padding=1))
+            nn.ConvTranspose2d(in_channels=256, out_channels=out_channels, kernel_size=4, stride=2, padding=1)
+        ]
+        layers = [l for l in layers if l]
+        self.main_module = nn.Sequential(*layers)
             # output of main module --> Image (Cx32x32)
 
         self.output = nn.Tanh()
