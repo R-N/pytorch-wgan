d75209b152ce2d9f62350fa8b09c29756298e104
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-24 13:34:34 +0700

    Revert "Update wgan_gradient_penalty.py"
    This reverts commit 06c28435be65bca121e745074710823c5801774a.

--- models/wgan_gradient_penalty.py
+++ models/wgan_gradient_penalty.py
@@ -202,18 +202,12 @@ class WGAN_GP(nn.Module):
                 d_loss_real = self.D(images)
                 d_loss_real = d_loss_real.mean()
 
-                d_loss_real.backward(mone)
-
                 # Train with fake images
                 z = self.get_torch_variable(torch.randn(self.batch_size, 100, 1, 1))
 
                 fake_images = self.G(z)
                 d_loss_fake = self.D(fake_images)
                 d_loss_fake = d_loss_fake.mean()
-
-                d_loss_fake.backward(one)
-
-                d_loss_grad = self.get_gradients().clone()
 
                 # Train with gradient penalty
                 #fake_images.requires_grad_(True)
@@ -222,8 +216,13 @@ class WGAN_GP(nn.Module):
                 gradient_penalty = self.calculate_gradient_penalty(images, fake_images)
                 gradient_penalty.backward()
 
+                gp_grad = self.get_gradients().clone()
+
+                d_loss_real.backward(mone)
+                d_loss_fake.backward(one)
+
                 grad = self.get_gradients().clone()
-                gp_grad = grad - d_loss_grad
+                d_loss_grad = grad - gp_grad
 
                 d_loss_grad = reduce_grad(d_loss_grad)
                 gp_grad = reduce_grad(gp_grad)
