3a5df9b94e451eb4b091f90e2a0730e1e47376eb
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-24 19:19:31 +0700

    Update wgan_gradient_penalty.py

--- models/wgan_gradient_penalty.py
+++ models/wgan_gradient_penalty.py
@@ -21,6 +21,7 @@ class Generator(torch.nn.Module):
         # Filters [1024, 512, 256]
         # Input_dim = 100
         # Output_dim = C (number of channels)
+        norm=True
         layers = [
             # Z latent vector 100
             nn.ConvTranspose2d(in_channels=in_channels, out_channels=1024, kernel_size=4, stride=1, padding=0),
@@ -64,20 +65,20 @@ class Discriminator(torch.nn.Module):
             # Image (Cx32x32)
             nn.Conv2d(in_channels=channels, out_channels=256, kernel_size=4, stride=2, padding=1),
             #nn.LayerNorm([16, 16], elementwise_affine=True),
-            #nn.InstanceNorm2d(256, affine=True),
-            nn.LeakyReLU(0.2),
+            nn.InstanceNorm2d(256, affine=True),
+            nn.LeakyReLU(0.2, inplace=True),
 
             # State (256x16x16)
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1),
             #nn.LayerNorm([8, 8], elementwise_affine=True),
-            #nn.InstanceNorm2d(512, affine=True),
-            nn.LeakyReLU(0.2),
+            nn.InstanceNorm2d(512, affine=True),
+            nn.LeakyReLU(0.2, inplace=True),
 
             # State (512x8x8)
             nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1),
             #nn.LayerNorm([4, 4], elementwise_affine=True),
-            #nn.InstanceNorm2d(1024, affine=True),
-            nn.LeakyReLU(0.2)
+            nn.InstanceNorm2d(1024, affine=True),
+            nn.LeakyReLU(0.2, inplace=True)
         ]
         layers = [l for l in layers if l]
         self.main_module = nn.Sequential(*layers)
